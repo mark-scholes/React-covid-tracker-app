4403724460dfca3b9774c71b9329136334e6f042
mark-scholes <innominatus15@outlook.com>
2020-08-04 03:41:29 +0100

    Added the header and a dropdown menu so the user can select a country. This list is populated from the disease api so it is a bit slow atm. Might be better to use something stored in state so it there is not a wait for the call to me made every time the drop down box is opened in the future.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
-function App() {
-  return <div className="App">Test</div>;
-}
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const changeCountry = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>covid-19 tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={changeCountry}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {
+        //info boxes
+        //cases
+        //recovered
+        //deaths
+        //end of info boxes
+        //sidebar
+        //graphs
+        //live cases by country
+        //worldwide new cases
+        //end of sidebar
+        //map
+      }
+    </div>
+  );
+};
 
 export default App;

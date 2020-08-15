5d42afcd5906a48ce7779e7c56bbb07991feb032
mark-scholes <innominatus15@outlook.com>
2020-08-15 01:36:51 +0100

    implemented the graph. Have used numeral to format the data when the user hovers over a pont on the graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,35 +1,100 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import numeral from "numeral";
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRation: false,
+  tooltips: {
+    made: "index",
+    interect: false,
+    callback: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
 
-const Graph = () => {
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+const Graph = ({ casesType = "cases" }) => {
   const [data, setData] = useState({});
 
   useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-      .then((response) => response.json())
-      .then((data) => {});
-  }, []);
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          let chartData = buildChartData(data, "cases");
+          setData(chartData);
+        });
+    };
 
-  const buildChartData = (data, casesType = "cases") => {
-    const chartData = [];
-    let lastDataPoint;
-    data[casesType].forEach((date) => {
-      if (lastDataPoint) {
-        const newDataPoint = {
-          x: date,
-          y: data["cases"][date] - lastDataPoint,
-        };
+    fetchData();
+  }, [casesType]);
 
-        chartData.push(newDataPoint);
-      }
-      lastDataPoint = data["cases"][date];
-    });
-    return chartData;
-  };
   return (
     <div>
       <h1>Monkey</h1>
-      {/* <Line data options /> */}
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                data: data,
+                backgroundColor: "rgba(204,16,52,0.5",
+                borderColor: "#CC1034",
+              },
+            ],
+          }}
+        />
+      )}
     </div>
   );
 };
